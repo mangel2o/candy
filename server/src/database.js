7ce3ea30002dc456ad17d34d29843c36988e3876
mangel2o <55473177+mangel2o@users.noreply.github.com>
2022-01-26 08:35:32 -0600

    Added mongo connection

--- server/src/database.js
+++ server/src/database.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 // TODO: Create own mongoDB connection
 // * To install mongoDB locally, follow this guide https://youtu.be/3wqzr-GJoS0
-mongoose.connect("")
+// * Mongoose is an ODM for mongoDB, it makes life easier 👍
+mongoose.connect("mongodb://localhost/candy-test")
    .then(db => console.log("Db is connected"))
    .catch(err => console.log(err));
