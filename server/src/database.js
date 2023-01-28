0a1eecd0550073ed2a375f872a8c0faee3639dfc
mangel2o <mangelcg.2o@outlook.com>
2023-01-28 00:27:05 -0600

    Changed localhost to ip

--- server/src/database.js
+++ server/src/database.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 // TODO: Create own mongoDB connection
 // * To install mongoDB locally, follow this guide https://youtu.be/3wqzr-GJoS0
 // * Mongoose is an ODM for mongoDB, it makes life easier 👍
-mongoose.connect("mongodb://localhost/candy-test")
+mongoose.connect("mongodb://127.0.0.1/candy-test")
    .then(db => console.log("Db is connected"))
    .catch(err => console.log(err));
